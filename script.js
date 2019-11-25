056b73c0e5f985196d2b36e9c60dfe30b77093c7
Abhishek Samariya <asamariya@gmail.com>
2019-11-26 00:09:17 +1100

    navbar and banner now behaving as they should

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
+const line = document.querySelectorAll('.hamburger .line');
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('open');
+  hamburger.classList.toggle('open');
+
+  line.forEach(l => l.classList.toggle('open'));
   links.forEach(link => {
     link.classList.toggle('fade');
   });
