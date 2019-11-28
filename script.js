0f010074065584924e1b2e0a41487ffb46660e7d
Abhishek Samariya <asamariya@gmail.com>
2019-11-28 14:30:50 +1100

    Added blurred background to top nav bar on scroll

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
+const topNavBar = document.querySelector('.menu');
 const links = document.querySelectorAll('.nav-links li');
 const line = document.querySelectorAll('.hamburger .line');
 const contentTitle = document.querySelector('.content-text h3');
@@ -13,4 +14,12 @@ hamburger.addEventListener('click', () => {
   links.forEach(link => {
     link.classList.toggle('fade');
   });
+});
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY === 0) {
+    topNavBar.classList.remove('active');
+  } else {
+    topNavBar.classList.add('active');
+  }
 });
