57f725e05850c1e8f33f5164858e499fbeb698e2
Abhishek Samariya <asamariya@gmail.com>
2019-11-28 14:08:25 +1100

    Updated z-index of main title content when hamburger menu is open

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
 const line = document.querySelectorAll('.hamburger .line');
+const contentTitle = document.querySelector('.content-text h3');
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('open');
   hamburger.classList.toggle('open');
+  contentTitle.classList.toggle('open');
 
   line.forEach(l => l.classList.toggle('open'));
   links.forEach(link => {
